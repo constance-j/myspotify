d164c223bd9680d769350598bfddbad8b6d67ea3
Constance James <constance.anastasia@gmail.com>
2017-01-16 11:53:33 +0100

    Add some files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+#db/seeds.rb
 
-require "seeds"
+# require "seeds"
 1000.times do |i|
     puts "Creating Track number #{i}"
     Track.create({title: "Awesome track #{i}"})
+
+    $text = 'Awesome track '.$i;
+    $text = "Awesome track $i";
 end
